eec0cae0008ca58022c324009c6696f29ef0870b
GIOkafor <ikechukwuokafor@hotmail.co.uk>
2020-10-03 14:08:53 -0400

    welcome screen and product view

--- App.js
+++ App.js
@@ -1,36 +1,85 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-} from "react-native";
-import { useDeviceOrientation } from "@react-native-community/hooks";
+import { View, Image, ImageBackground, Text } from "react-native";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
+  /*
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
+      <ImageBackground
+        style={{
+          flex: 8,
+          alignItems: "center",
+          paddingTop: 50,
+        }}
+        source={require("./assets/background.jpg")}
+      >
+        <Image
+          source={require("./assets/logo-red.png")}
+          style={{
+            height: 100,
+            width: 100,
+          }}
+        />
+        <Text>Sell What You Don't Need</Text>
+      </ImageBackground>
+
       <View
         style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
+          backgroundColor: "gold",
+          flex: 1,
         }}
-      ></View>
-    </SafeAreaView>
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          flex: 1,
+        }}
+      />
+    </View>
+  );
+  */
+
+  return (
+    <View
+      style={{
+        backgroundColor: "black",
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          padding: 10,
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "tomato",
+            height: 50,
+            width: 50,
+          }}
+        />
+        <View
+          style={{
+            backgroundColor: "green",
+            height: 50,
+            width: 50,
+          }}
+        />
+      </View>
+      <Image
+        source={require("./assets/chair.jpg")}
+        style={{
+          height: 500,
+          alignSelf: "center",
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
